f72261b8119cc4852fad40e8d150c67ae99887a8
Andrew Wrenn <awrenn53@gmail.com>
2019-05-25 06:34:48 -0700

    Update

--- install.sh
+++ install.sh
@@ -9,12 +9,20 @@ function vimrc {
 
 function byobu {
     cp tmux.conf ~/.byobu/.tmux.conf
+    cp color.tmux ~/.byobu/color.tmux
 }
 
 function zsh {
     wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O /tmp/oh_my_zsh.sh
     sh "/tmp/oh_my_zsh.sh"
     cp zshrc ~/.zshrc
+}
+
+function gnome {
+    profile=$(gsettings get org.gnome.Terminal.ProfilesList default)
+    profile=${profile:1:-1} # remove leading and trailing single quotes
+    gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" background-color '#00001a'
+    gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" foreground-color '#ffffcc'
 }
 
 case $1 in
@@ -26,6 +34,9 @@ case $1 in
     ;;
     zsh*)
         zsh
+    ;;
+    gnome*)
+        gnome
     ;;
     all*)
         vimrc
