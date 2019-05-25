780cf447b88693f0149507a8d1826bc92624bdf3
Andrew Wrenn <awrenn53@gmail.com>
2019-05-25 05:44:50 -0700

    Update

--- install.sh
+++ install.sh
@@ -1,4 +1,36 @@
-cp .vimrc ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-## Now run :PluginInstall
-#python2 ~/.vim/plugins/YouCompleteMe/install.py
+#!/bin/bash
+
+function vimrc {
+    cp vimrc ~/.vimrc
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim -c :PluginInstall -c :q -c :q
+    python2 $(find ~/.vim/plugins -name install.py | grep YouCompleteMe)
+}
+
+function byobu {
+    cp tmux.conf ~/.byobu/.tmux.conf
+}
+
+function zsh {
+    wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O /tmp/oh_my_zsh.sh
+    zsh -c /tmp/oh_my_zsh.sh
+    cp zshrc ~/.zshrc
+}
+
+case $1 in
+    vimrc*)
+        vimrc
+    ;;
+    byobu*)
+        byobu
+    ;;
+    zsh*)
+        zsh
+    ;;
+    all*)
+        vimrc
+        byobu
+        zsh
+    ;;
+esac
+
